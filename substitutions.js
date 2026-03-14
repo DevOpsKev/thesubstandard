8cf0ec0e428cf8cd0c93bc7e972cf0bc3562a94c
Kevin Ryan <kevinryan1973@gmail.com>
2026-03-14 11:26:45 +0000

    Update substitutions.js

--- substitutions.js
+++ substitutions.js
@@ -88,7 +88,7 @@ const DEFAULT_SUBS = [
   ["google",                             "Bing"],
   ["twitter",                            "Der Angriff"],
   ["x (formerly twitter)",               "Der Angriff"],
-  ["Middle East",                    "Hunger Games"],
+  ["Middle East",                        "Hunger Games"],
 
   // ── UK-Specific ───────────────────────────────────────────
   ["Downing Street",                     "the head prefect's bedsit"],
